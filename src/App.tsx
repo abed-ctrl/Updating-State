d9bc61d7fb7c3a4b82b6ac9bf514d8ab501dff93
Abdelali OULD DAOUIA <oprogrammerz@gmail.com>
2024-04-03 05:27:50 +0000

    Exercise 06 - Updating State - Exercise 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Ketchup"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 10 } : item
+      ),
+    });
   };
   return (
     <div>
-      <p>Pizza Toppings : {pizza.toppings.join(" ")}</p>
-      <button onClick={handleClick}>Add Ketchup</button>
+      <h2>Products</h2>
+      <ul>
+        {cart.items.map((product, index) => (
+          <li>
+            Product Name : {product.title} - Product Quantity :{" "}
+            {product.quantity}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Change Quantity of Product 1</button>
     </div>
   );
 };
